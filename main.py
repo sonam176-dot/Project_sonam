e1a751de46170901527ba169059448904c5c9c52
sonam176-dot <modisonu176@gmail.com>
2025-11-14 12:35:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,501 @@
+# print("hello world")
+# x= 2.5
+# print(type(x))
+# x= 4.5
+# y= 4.5
+# print(x+y)
+# print(type(x+y))
+# x=10
+# y="baby"
+# print(x*y)
+# x=20
+# y=80
+# print(x+y)
+# print(x-y)
+# print(x/y)
+# print(x*y)
+# print(x**y)
+# x="jnwjsnwjbdhwdjowjwijioqwjsiwqjskwjwkjjqjiqjiowqjoeqjqjdnjdwnj;whndjhje                   sjnxjknkjnjlnkjdncjwncj"
+# print(len(x))
+# x=40
+# y=3.4
+# z="sonam"
+# u= "we are learning python"
+# print(x+y)
+# print(z+u)
+# print(x)
+# my_list=["banana", "apple", 3,4 ,8 ,0]
+# my_list[2:5]="abc"
+# print(my_list)
+# my_list=[1,2,4,5,9]
+
+# list1 =[1,2,3,4,5]
+# list2 =[ 6,7,8,9]
+# list1.extend(list2)
+# print(list1)
+
+
+
+
+# list1 =[1,2,3,4,5]
+# list1.remove(5)
+# print(list1)
+
+# list1 =["app","ind","yuo" ,"jk"]
+# list1.pop()
+# print(list1)
+
+
+
+# list1=[1,2,3,4,5,"app","banana","cat"]
+# #now using remove,pop,extend function one by one...
+# list1.remove("app")
+# print(list1)
+# list1.pop()
+# print(list1)
+
+# list1=[1,2,3,4,5,"app","banana","cat"]
+# list2 =[ 6,7,8,9]
+# list1.extend(list2)
+# print(list1)
+
+# list2 =[ 6,7,8,9]
+# list2.insert( 3,"sonam")
+# print(list2)
+
+# list2 =[ 6,7,8,9]
+# list2.append( 3)
+# print(list2)
+
+# list2.clear()
+# print(list2)
+
+# list1=[1,2,3,4,4,4,5,6]
+# for y in list1:
+#     print(y)
+    
+#  #sort
+# list=[2,1,6,3,6,78,67,0]
+# list.sort()
+# print(list)
+    
+# list=[2,1,6,3,6,78,67,0]
+     
+# list.sort(reverse=True)
+# print(list)
+    
+    
+    
+# text ="hello python"   
+# print(text.upper())
+# print(text.lower())
+# print(text.strip())
+# print(text.replace("python","world"))
+# print(text.split())
+
+# name="bob"
+# age=8
+# print(x+y)
+
+
+# x="bob"
+# y=8
+# print(f"my name is{x} and i am {y}")
+
+# x=5
+# x+=3
+# print(x)
+
+# x=5
+# x/=3
+# print(x)
+
+# x=5
+# x-=3
+# print(x)
+
+# x=5
+# x*=3
+# print(x)
+
+# x=4
+# x==1
+# print(x)
+
+# x=5
+# y=10
+# print(x==y)
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x<=y)
+# print(x>=y)
+
+# x=8
+# y=x
+# z=y
+# print(y)
+
+
+# colors={"red" ,"blue" ,"green","yellow"}
+# print(colors)
+# colors.add("pink")
+# print(colors)
+
+# colors. remove("green")
+# print(colors)
+
+# for x in colors:
+#     print(x)
+    
+# #dictionaries # if else
+   
+# age=20
+# if age <=18: print("you are eligible to vote") 
+# else: (" you are not eligible")
+
+# age=18
+# if age >=18: print("you are eligible to vote") 
+# else:(" you are not eligible")
+
+
+# # if elf else
+
+# marks= 92
+
+# if marks>= 90:
+#  print("GRADE : A+")
+# elif marks>= 75:
+#  print(" GRADE :A")
+# elif marks>= 
+    
+    
+#     60:
+#     print(" GRADE :B")
+    
+# else:
+#     print("GRADE: C")
+# x= 5
+    
+# if x>10:
+#     print("x is greater than 10")
+        
+# if x>20: 
+#     print("x is greater than 20")
+        
+# else:
+#       print("x is not greater than 20")
+      
+      
+# x= 15
+      
+# if x>18:
+#           print("you are eligible")
+          
+# elif x>=15:
+#     print(" you are also eligible")
+          
+# else:
+#           print(" you are not eligible")         
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+# x= 15
+ 
+# if x%2==0:
+#  print("even")
+ 
+# else:
+#     print("odd")
+    
+    
+    
+#     #loops
+    
+    
+    
+ 
+# word = "python"
+ 
+# for x in word:
+#     print(x)
+    
+    
+ 
+
+
+
+# for i in range (1, 10 , 2):
+#     print(i)
+      
+    
+# for i in range(1,4):
+        
+         
+#  for j in range(1,3):
+    
+      
+#     print(f"i={i},j={j}")
+
+# for i in range(1 , 6):
+    
+#     if i==4:
+        
+#           break
+        
+#     print(i)
+    
+#     for i in range (1,21):
+#         print(i)
+        
+#         i = 1
+#         while i<=20:
+#             print (i)
+#             i+=1
+            
+#             for i in range(2 , 30 , 2):
+#                 print(i)
+  
+  
+  
+  
+  
+                
+                
+# word= "sonam modi"  
+
+
+
+# for i in word:
+#          print(i)
+         
+         # functions
+         
+         
+                   
+
+
+
+# function with return value
+
+# def add(a,b):
+#     return a + b
+                
+#     result =add(5,3)    
+#     print(result)
+
+# print("hello")
+
+# #class
+# class Car:
+#     def __init__(self,brand,color):
+#      self.brand = brand
+#      self.color = color
+#     def drive(self):
+# #      print(f"{self.color} {self.brand} is driving 🚗")
+        
+# car1 = Car("BMW","BLACK")
+# car2 = Car("TESLA","WHITE")
+
+
+# car1.drive()
+# car2.drive()
+
+# num = int(input("enter a number"))
+# if num > 0:
+#     print("positive number")
+    
+# elif num == 0:
+#     print("zero")
+    
+# else:
+#     print("negative number")
+
+
+#ARRAYS
+
+# import array
+
+
+# number=array.array[1,2,3,4,5]
+# print(number)
+
+# num = int(input("Emter a number"))
+# if num%2 ==0:
+#     print("EVEN NUMBER")
+    
+# else:
+#       print("ODD NUMBER")
+
+#from numpy import random
+
+
+
+# x= random.randint(100)
+# print(x)
+
+
+# x=random.rand()
+# print(x)
+
+
+# a=int(input(" Enter first number"))
+# b=int(input(" Enter second number"))
+
+
+
+# if  a>b:
+#     print("a is greater than b")
+    
+# elif a == b :
+#     print("a is equal to b")
+    
+# else:
+#     print("a is lesser than b")
+
+# list=[1,2,3,4,5,34,3,34,4556,456,4]
+# list.append(7)
+# print(list)
+
+# list=["apple","banana","nano"]
+# list.insert( -2,"sonam")
+# print(list)
+
+# list2 =[ 6,7,8,9]
+# list2.insert( 3,"sonam")
+# print(list2)
+
+
+# list2 =[ 6,7,8,9,9,7,6,56,987]
+# list2.sort()
+# print(list2)
+
+
+# list2 =[ 6,7,8,9,9,7,6,56,987]
+# list2.sort(reverse=True)
+# print(list2)
+
+
+# list2 =[ 6,7,8,9,9,7,6,56,987]
+# list2.pop()
+# print(list2)
+
+
+
+
+# num =int(input(" enter a number:"))
+# if num>=95:
+#     print("grade A")
+# elif 50<num>75:
+#      print("grade B")
+# else:
+#     print("grade C")
+     
+         
+# age=int(input("Enter a age"))
+# if age >=18:
+#         print("you are eligible to vote")
+        
+# else:
+#     print("you are not eligible")  
+
+# num= int(input("Enter a number :"))
+# if num % 5==0 and num % 11==0:
+#     print("number is divisible by both 5 and 11")
+    
+# else:
+#     print("number is  not divisible by both 5 and 11")
+
+#CREATING A SERIES
+
+import pandas as pd
+
+
+data=[10,0,30,40]
+s=pd.Series(data)
+
+print(s)
+
+import numpy as np
+
+
+arr = np.array([1,2,3,4,5])
+s=pd.Series(arr)
+print(s)
+    
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from numpy import random
+
+
+
+# x= random.randint(100 ,size=(3,5))
+# print(x)
+
+# x= random.randint(100 ,size=(5,4,8,2))
+# print(x)
+
+# list=[1,2,3,4,5,6,7,8]
+
+# for x in list:
+#     print(x)
+
+# list="sonam  modi "
+
+# for x in list:
+#     print(x)
+
+
+# a=int(input("Enter first number: "))
+# b=int(input("Enter second number: "))
+# c=int(input("Enter third number: "))
+
+# if a>=b and a>=c:
+#     print("a is a greater")
+    
+# elif a>=b and a>=c:
+#     print("b is a greater")
+    
+# else:
+#     print("c is greater")
+
+
+# year=int(input("enter a number"))
+# if year%4==0 and year % 100 != 0:
+#     print("This is a leap year")
+    
+# else:
+#      print("This is  not a leap year")
+
+
+# ch = input("Enter a character:") . lower() 
+# if ch in['a','e','i','o','u']:
+#     print("vowel")
+    
+# else:
+#     print("Consonant")
+
+
+
